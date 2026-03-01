33e769f2c4d421449d5fe9513a5d4309d11dacbc
Petrov Vadim <vadikdydy@mail.ru>
2026-03-01 19:50:02 +0300

    done

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <cstddef>
+
+template< class T >
+T* merge(const T* const* a, size_t sa, const size_t* sai, T* c)
+{
+  size_t i = 0, j = 0, k = 0;
+  while (i < sa)
+  {
+    while (j < sai[i])
+    {
+      c[k] = a[i][j];
+      k++;
+      j++;
+    }
+    i++;
+    j = 0;
+  }
+  return c;
+}
