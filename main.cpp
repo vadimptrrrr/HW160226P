dcbae015538f07dc255e4de8978d8092ebf952e3
Petrov Vadim <vadikdydy@mail.ru>
2026-03-02 11:03:06 +0300

    mergeN

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <iostream>
 
 template<class T>
 T* merge2(const T* a, size_t sa, const T* b, size_t sb, T* c)
@@ -30,17 +31,31 @@ T* merge2(const T* a, size_t sa, const T* b, size_t sb, T* c)
 template< class T >
 T* merge(const T* const* a, size_t sa, const size_t* sai, T* c)
 {
-  size_t i = 0, j = 0, k = 0;
+  for (size_t i = 0; i < sai[0]; ++i)
+  {
+    c[i] = a[0][i];
+  }
+
+  size_t i = 1, k = sai[0];
   while (i < sa)
   {
-    while (j < sai[i])
+    T* tmp = new T[k + sai[i]];
+    try
     {
-      c[k] = a[i][j];           //T::operator=
-      k++;
-      j++;
+      merge2(a[i], sai[i], c, k, tmp);
+      for(size_t j = 0; j < k + sai[i]; ++j)
+      {
+        c[j] = tmp[j];
+      }
+      k += sai[i];
+      delete[] tmp;
+    }
+    catch (...)
+    {
+      delete[] tmp;
+      throw;
     }
     i++;
-    j = 0;
   }
   return c;
 }
