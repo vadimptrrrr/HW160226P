fa832fba4a5a43532f63091cb47bfe4d6e68cd42
Petrov Vadim <vadikdydy@mail.ru>
2026-03-02 10:28:43 +0300

    merge2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include <cstddef>
 
+template<class T>
+T* merge2(const T* a, size_t sa, const T* b, size_t sb, T* c)
+{
+  size_t i = 0, j = 0, k = 0;
+  while (i < sa && j < sb)
+  {
+    if (a[i] <= b[j])
+    {
+      c[k++] = a[i++];
+    }
+    else
+    {
+      c[k++] = b[j++];
+    }
+  }
+  while (i < sa)
+  {
+    c[k++] = a[i++];
+  }
+
+  while (j < sb)
+  {
+    c[k++] = b[j++];
+  }
+  return c;
+}
+
 template< class T >
 T* merge(const T* const* a, size_t sa, const size_t* sai, T* c)
 {
